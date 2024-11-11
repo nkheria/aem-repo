b9283dcc131cab6d8b77a9e4d4954ca2808013b4
Neeraj <nkheria@gmail.com>
2024-11-11 07:03:33 +0000

    junit test cases added

---
+++ core/src/main/java/com/mysite/core/models/impl/ByLineImplTest.java
@@ -0,0 +1,29 @@
+package com.mysite.core.models.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEch;
+import org.junit.jupiter.api.Test;
+
+public class ByLineImplTest {
+
+    @BeforeEach void setUp() throws Exception {
+
+    }
+
+    @Test
+    void testGetName() {
+        fail("Not yet implemented")
+    }
+
+    @Test
+    void testGetOccupations() {
+        fail("Not yet implement")
+    }
+
+    @Test
+    void testIsEmpty() {
+        fail("Not yet implemented")
+    }
+
+}
